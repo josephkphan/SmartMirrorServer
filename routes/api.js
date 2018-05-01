8496b9f179f25136a2d9e4819928758c8be736fd
thomasvn <thomasnguyen96@gmail.com>
2018-04-30 22:24:08 -0700

    Started on the POST request to the web server

--- routes/api.js
+++ routes/api.js
@@ -10,10 +10,16 @@ var User = require('../models/user');
 router.get('/get_user_settings', function(req, res) {
     res.setHeader('Content-Type', 'application/json');
 
+    // Double check that correct arguments were passed
+    if (!('username' in req.query) || !('mirrorID' in req.query)) {
+        console.log("Invalid arguments passed to /get_user_settings request");
+        res.send("/get_user_settings Request Failed: Invalid Arguments")
+    }
+
     // TODO: Don't get user by username, get user by Mirror ID ... If it doesn't exist, return error "web account not set up yet"
 
-    User.getUserByUsername(req.query.username, function(err, user) {  // TODO: Change to be inside the params key as opposed to the query key
-        if(err) throw err;
+    User.getUserByUsername(req.query.username, function(err, user) {
+        if(err) console.log(err);
 
         // Check if MirrorIDs are the same
         if (req.query.mirrorID === user.mirrorID) {  // TODO: Check to make sure that there is a MirrorID ... Authenticating with FB will skip this step
@@ -24,13 +30,36 @@ router.get('/get_user_settings', function(req, res) {
 
 /**
  * Update's a user's settings on the web server. Requires an HTTP request with:
- * { username: string, mirrorID: string }
+ * Params: { username: string, mirrorID: string }
+ * Body: { color: string, fontSize: string }
  */
-// router.post('/update_user_settings', function(req, res) {
-//     // Verify which user this is
+router.post('/update_user_settings', function(req, res) {
+    // Double check that correct arguments were passed
+    if (!('username' in req.query) || !('mirrorID' in req.query)) {
+        console.log("Invalid arguments passed to /update_user_settings request");
+        res.send("/update_user_settings Request Failed: Invalid Arguments")
+    }
 
-//     // TEST
-// });
+    // Get username info
+    User.getUserByUsername(req.query.username, function(err, user) {
+        if(err) console.log(err);
+
+        // Check if MirrorIDs are the same
+        if (req.query.mirrorID != user.mirrorID) {
+            res.send('INVALID MirrorID or Username');
+            return;
+        }
+
+        // Read the JSON from req.body
+        updates = req.body;
+
+        // Update the schema for this user
+        // updates.color
+        // updates.fontSize
+
+        res.send('POST Request sent to update user settings');
+    });
+});
 
 
 module.exports = router;
